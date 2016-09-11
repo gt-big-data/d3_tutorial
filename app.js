8c1c25064a1714ed24f1193dd937ea57f10f516f
Faisal Animashaun <d.f.animashaun@gmail.com>
2016-09-11 16:10:59 -0400

    finished plottable

--- app.js
+++ app.js
@@ -7,24 +7,15 @@ var svg = d3.select('body')
     .attr('height', HEIGHT + margin.top + margin.bottom)
     .attr('width', WIDTH + margin.left + margin.right);
 
-var chart = svg.append('g')
-    .attr('class', 'chart')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-    .attr('width', WIDTH)
-    .attr('height', HEIGHT);
+var xScale =  new Plottable.Scales.Linear();
+var yScale = new Plottable.Scales.Linear();
 
-var xScale = d3.scale.linear()
-  .range([0, WIDTH]);
+var xAxis = new Plottable.Axes.Numeric(xScale, "bottom");
+var yAxis = new Plottable.Axes.Numeric(yScale, "left");
 
-var yScale = d3.scale.linear()
-  .range([HEIGHT, 0]);
-
-var rScale = d3.scale.linear()
-  .range([3, 6]);
-
-var tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
+var plot = new Plottable.Plots.Scatter()
+    .x(function(d) { return d.Attack; }, xScale)
+    .y(function(d) { return d.Defense; }, yScale);
 
 function type(d) {
   d.Attack = +d.Attack;
@@ -37,79 +28,24 @@ d3.csv('pokemon.csv', type, function(error, data) {
   if (error) {
     return;
   }
+  var dataset = new Plottable.Dataset(data);
+  plot.addDataset(dataset);
 
-  var xMax = d3.max(data, function(d) {return d.Attack;});
-  xScale.domain([0, xMax]);
+  var chart = new Plottable.Components.Table([
+    [yAxis, plot],
+    [null, xAxis]
+  ]);
 
-  var yMax = d3.max(data, function(d) {return d.Defense;});
-  yScale.domain([0, yMax]);
+  chart.renderTo(svg);
 
-  var rMax = d3.max(data, function(d) {return d.HP});
-  rScale.domain([0, rMax]);
+  new Plottable.Components.AxisLabel("Defense")
+  .yAlignment('top')
+  .xAlignment("left")
+  .angle(90)
+  .renderTo(svg);
 
-  var xAxis = d3.svg.axis()
-      .scale(xScale)
-      .orient('bottom');
-
-  chart.append('g')
-      .attr('transform', 'translate(0,' + HEIGHT + ')')
-      .attr('class', 'axis date')
-      .call(xAxis)
-      .append("text")
-      .attr("class", "label")
-      .attr("x", WIDTH)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Attack");;
-
-  var yAxis = d3.svg.axis()
-      .scale(yScale)
-      .orient('left');
-
-  chart.append('g')
-      .attr('transform', 'translate(0, 0)')
-      .attr('class', 'axis date')
-      .call(yAxis)
-      .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 15)
-      .style("text-anchor", "end")
-      .text("Defense");
-
-  chart.selectAll('.dots')
-      .data(data)
-      .enter()
-      .append("svg:circle")
-      .attr('cx', function(d) {
-        return xScale(d.Attack);
-      })
-      .attr('cy', function(d) {
-         return HEIGHT - yScale(d.Defense);
-      })
-      .attr('r', function(d) {
-        return rScale(d.HP);
-      })
-      .attr('fill', function(d) {
-        return d.Legendary == "True" ? '#2ecc71': 'steelblue';
-      })
-      .on('mouseover', function(d) {
-        d3.select(this)
-          .attr('r', 8);
-
-        tooltip.transition()
-                .duration(200)
-                .style("opacity", .9);
-        tooltip.html(d.Name)
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 28) + "px");
-      })
-      .on('mouseout', function(d) {
-        d3.select(this)
-          .attr('r', rScale(d.HP));
-          
-        tooltip.transition()
-            .duration(500)
-            .style("opacity", 0);
-      })
-})
+  new Plottable.Components.AxisLabel("Attack")
+  .yAlignment('bottom')
+  .xAlignment("right")
+  .renderTo(svg);
+});
